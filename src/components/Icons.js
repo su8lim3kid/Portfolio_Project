1c0bf8616dcde7e302835cfe209ff7025e6f7bcb
su8lim3kid <aldereteangel21@gmail.com>
2019-12-23 16:44:50 -0700

    updated links

--- src/components/Icons.js
+++ src/components/Icons.js
@@ -1,32 +1,25 @@
 import React  from 'react'
 import { Icon,  } from 'semantic-ui-react'
+import { Link, } from 'react-router-dom';
 
 function Icons(){
 
-  function sayGithub() {
-    alert('GitHub!');
-  }
 
-  function sayFacebook() {
-    alert('Facebook!');
-  }
 
   function sayLinkdin() {
     alert('Linkdin!');
   }
 
-  function sayInstagram() {
-    alert('Instagram!');
-  }
-
+ 
 return(
+
 
 <>
 
-    <Icon circular inverted color='teal' name='github basic icon' onClick={sayGithub} />
-    <Icon circular inverted color='teal' name='facebook official'onClick={sayFacebook} />
-    <Icon circular inverted color='teal' name='linkedin' onClick={sayLinkdin}/>
-    <Icon circular inverted color='teal' name='instagram'onClick={sayInstagram} />
+   <Link to="/https://github.com/su8lim3kid"> <Icon circular inverted color='teal' name='github '/></Link>
+   <Link to="/https://www.facebook.com/angel.alderete.374"> <Icon circular inverted color='teal' name='facebook official' /></Link>
+    <Link> <Icon circular inverted color='teal' name='linkedin' onClick={sayLinkdin}/></Link>
+
    
   
 </>
